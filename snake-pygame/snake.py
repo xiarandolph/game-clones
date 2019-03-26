f850740bd78031a097c6384f7bd15bb703be515d
Randolph Xia <xia.randolph.227@gmail.com>
2019-03-26 18:22:46 -0400

    Add start & game over screen with simple score tracking

--- snake-pygame/snake.py
+++ snake-pygame/snake.py
@@ -31,16 +31,7 @@ LEFT = 'left'
 DOWN = 'down'
 RIGHT = 'right'
 
-def main():
-    global DISPLAY, CLOCK
 
-    pygame.init()
-    DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    CLOCK = pygame.time.Clock()
-    pygame.display.set_caption('Snake')
-
-    while True:
-        run_game()
 
 def snake_collided():
     # check for colliding with end of map
@@ -60,8 +51,9 @@ def draw_snake():
         pygame.draw.rect(DISPLAY, GREEN, (x, y, CELL_SIZE, CELL_SIZE))
 
 def run_game():
-    global snake
+    global snake, SCORE
 
+    SCORE = 0
     # get starting location
     start_x = randint(3, GAME_WIDTH - 4)
     start_y = randint(3, GAME_HEIGHT - 4)
@@ -69,7 +61,7 @@ def run_game():
                 {'x': start_x - 1, 'y': start_y},
                 {'x' : start_x - 2, 'y': start_y}]
     # starting direction
-    dir = RIGHT
+    direction = RIGHT
     new_dir = RIGHT
 
     # get random location for food
@@ -87,18 +79,18 @@ def run_game():
             # uses a new_dir in order to not be able to go backwards
             # if keys are pressed fast enough in succession
             elif event.type == KEYDOWN:
-                if event.key in UP_KEYS and dir != DOWN:
+                if event.key in UP_KEYS and direction != DOWN:
                     new_dir = UP
-                elif event.key in LEFT_KEYS and dir != RIGHT:
+                elif event.key in LEFT_KEYS and direction != RIGHT:
                     new_dir = LEFT
-                elif event.key in DOWN_KEYS and dir != UP:
+                elif event.key in DOWN_KEYS and direction != UP:
                     new_dir = DOWN
-                elif event.key in RIGHT_KEYS and dir != LEFT:
+                elif event.key in RIGHT_KEYS and direction != LEFT:
                     new_dir = RIGHT
                 elif event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        dir = new_dir
+        direction = new_dir
 
         if (snake_collided()):
             print("game over")
@@ -107,21 +99,23 @@ def run_game():
         # if food is eaten, generate a new location
         if snake[0]['x'] == food['x'] and snake[0]['y'] == food['y']:
             food = {'x': randint(0, GAME_WIDTH - 1), 'y': randint(0, GAME_HEIGHT - 1)}
+            SCORE += 1
         # else, remove tail
         else:
             snake.pop()
 
         # create new head in direction
-        if dir == UP:
+        if direction == UP:
             snake.insert(0, {'x': snake[0]['x'], 'y': snake[0]['y'] - 1})
-        elif dir == LEFT:
+        elif direction == LEFT:
             snake.insert(0, {'x': snake[0]['x'] - 1, 'y': snake[0]['y']})
-        elif dir == DOWN:
+        elif direction == DOWN:
             snake.insert(0, {'x': snake[0]['x'], 'y': snake[0]['y'] + 1})
-        elif dir == RIGHT:
+        elif direction == RIGHT:
             snake.insert(0, {'x': snake[0]['x'] + 1, 'y': snake[0]['y']})
 
         DISPLAY.fill(BLACK)
+        # draw the food
         pygame.draw.rect(DISPLAY, RED, (food['x'] * CELL_SIZE, food['y'] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
         draw_snake()
 
@@ -129,6 +123,53 @@ def run_game():
         # limit the game speed to FPS
         CLOCK.tick(FPS)
 
+def show_start_screen():
+    DISPLAY.fill(BLACK)
+    text = FONT.render("Press any key to begin", True, WHITE)
+    DISPLAY.blit(text, (WINDOW_WIDTH // 2 - text.get_width() // 2, WINDOW_HEIGHT // 2))
+    pygame.display.update()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                return
+
+def show_game_over():
+    DISPLAY.fill(BLACK)
+    text = FONT.render("GAME OVER!", True, WHITE)
+    DISPLAY.blit(text, (WINDOW_WIDTH // 2 - text.get_width() // 2, WINDOW_HEIGHT // 3))
+    text = FONT.render("SCORE: " + str(SCORE), True, WHITE)
+    DISPLAY.blit(text, (WINDOW_WIDTH // 2 - text.get_width() // 2, WINDOW_HEIGHT // 3 + text.get_height()))
+    text = FONT.render("Press any key to restart", True, WHITE)
+    DISPLAY.blit(text, (WINDOW_WIDTH // 2 - text.get_width() // 2, WINDOW_HEIGHT // 3 + 2 * text.get_height()))
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                return
+    
+
+def main():
+    global DISPLAY, CLOCK, FONT, SCORE
+
+    pygame.init()
+    DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    FONT = pygame.font.Font(None, 24)
+    SCORE = 0
+    pygame.display.set_caption('Snake')
+
+    show_start_screen()
+    while True:
+        run_game()
+        show_game_over()
 
 if __name__ == '__main__':
     main()
